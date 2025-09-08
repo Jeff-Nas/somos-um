c57d203b39fe9d0fcd5c1ec4ac96aabeae399a46
Jeferson Nascimento <jeferson.ifce@gmail.com>
2025-09-08 18:19:56 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css'
+import { StudyContent } from './StudyContent'
+import { Header } from './Header'
+
+function App() {
+
+
+  return (
+    <main className='h-full'>
+      <Header />
+      <StudyContent />
+    </main>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+
+export function Header() {
+    return (
+        <div>
+            <div className="flex items-center justify-around bg-gray-950 text-white p-3">
+                <h1 className="text-2xl font-montserrat font-bold">Somos Um</h1>
+                <img className="w-12" src="ccvideira.png" alt="Logo CCVideira" />
+            </div>
+            <img src="baner-ancora.png" alt="banner" />
+            <hr className="border-b-1 my-2 border-gray-600"></hr>
+        </div>
+    )
+}
